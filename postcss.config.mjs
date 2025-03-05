e02a478d212c65ce73d3d8a37cb7f456a32c5e63
minkaizen <martin.g.cao@gmail.com>
2025-03-06 08:35:13 +1100

    build: convert rems to px in tailwind styles

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -4,6 +4,7 @@ import noTwConflict from './postcss-plugin-no-tw-conflict.mjs';
 import autoprefixer from 'autoprefixer';
 import postcssNested from 'postcss-nested';
 import postcssUrl from 'postcss-url';
+import remToPxPlugin from './postcss-plugin-rem-to-px.mjs';
 
 export default {
   plugins: [
@@ -19,6 +20,15 @@ export default {
     noTwConflict({
       twSelector: '.tw',
       excludeFiles: ['tailwind.css'],
+    }),
+    remToPxPlugin({
+      rootValue: 16,
+      unitPrecision: 5,
+      propList: ['*'],
+      selectorWhiteList: ['tw'],
+      replace: true,
+      mediaQuery: true,
+      includeFiles: ['tailwind.css'],
     }),
     autoprefixer(),
     postcssNested(),
